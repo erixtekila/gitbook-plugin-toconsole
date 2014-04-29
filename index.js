8f5a05490656f27189fef06ccf5c1b8057840afd
Eric Priou <erixtekila@gmail.com>
2014-04-30 00:41:18 +0200

    0.0.2 Add eval buttons at build time

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-module.exports = {
-    book: {
-        assets: "./book",
-        js: [
-            "plugin.js"
-        ]
-    }
+var cheerio = require( "cheerio" )
+	,$
+	,buttontext = "Evaluate in console"
+	;
+
+module.exports =
+{
+	book:
+	{
+		assets: "./book"
+	}
+	,hooks:
+	{
+		// This is called for each page of the book
+		// It can be used for modifing page content
+		// It should return the new page
+		page: function ( page )
+		{
+			var section
+			;
+			for ( var i in page.sections )
+			{
+				section = page.sections[i];
+				if ( section.type == "normal" )
+				{
+					$ = cheerio.load( section.content );
+					$( ".lang-js" ).each
+					(
+						function()
+						{
+							var $this = $( this )
+								,$pre = $this.parent( "pre" )
+								;
+							$pre.after
+							(
+								'<div class="js-toconsole clearfix" ><button class="btn pull-right" onclick="eval(\''+ $this.text().trim() +'\')">'+ buttontext +'</button></div>'
+							);
+
+							//FIXME Monkey patch cheerio escaping strings
+							// Replace by transform
+							section.content = $.html().replace(/&apos;/g, "'" );
+						}
+					);
+				}
+			}
+			return page;
+		}
+	}
 };
